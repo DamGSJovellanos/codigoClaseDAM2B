26def463ada7d239ec0db653eaf4699db24c6f24
Grado superior clase <grado.superior.dam.vincen@gmail.com>
2025-10-16 18:40:55 +0200

    jueves 16 de octubre 2025

--- AccesoADatos/UT2/EjerciciosSQL/conexion_db_003.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-with sqlite3.connect("chinook.sqlite") as conn:
-    cursor = conn.cursor()
-    cursor.execute(''' SELECT name FROM sqlite_master WHERE type='table' ''')
-    resultado = cursor.fetchall()
-
-    print(resultado)
